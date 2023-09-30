8207b9b0de2d72a70380bbd346f640470bfc224a
Ese Amadasun <amadasunese@gmail.com>
2023-09-30 13:48:57 +0100

    updated pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# a function def pascal_triangle(n): that returns a list of lists of integers
+""
+A function that returns a list of lists of integers
+""
 def pascal_triangle(n):
     if n <= 0:
         return []
