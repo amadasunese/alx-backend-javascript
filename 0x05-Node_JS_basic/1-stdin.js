2cb8c1c88d85be671080122e650344d1f68c3c4b
Ese Amadasun <amadasunese@gmail.com>
2023-11-21 12:42:29 +0100

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface for stdin and stdout
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Prompt the user
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // Output the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Close the readline interface
-  rl.close();
-});
-
-// Event listener for closing the program
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
