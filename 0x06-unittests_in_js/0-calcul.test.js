8c56d98b26b168e1377c537b887eed9099b92333
Ese Amadasun <amadasunese@gmail.com>
2023-11-23 15:38:21 +0100

    updated

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,23 +1,31 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', () => {
-    it('sums two rounded numbers - case 1', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+describe("calculateNumber()", function() {
+
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
     });
-
-    it('sums two rounded numbers - case 2', () => {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
     });
-
-    it('sums two rounded numbers - case 3', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
     });
-
-    it('sums two rounded numbers - case 4', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
     });
-
-    // Add more tests for edge cases and other scenarios
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
 });
-
